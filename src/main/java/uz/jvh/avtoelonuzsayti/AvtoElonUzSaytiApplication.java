233e03c8d477e73ba8115e593baba8b738b72af6
Javohir004 <javohirsharifjonov795@gmail.com>
2024-11-19 18:16:43 +0500

    Initial commit

---
+++ src/main/java/uz/jvh/avtoelonuzsayti/AvtoElonUzSaytiApplication.java
@@ -0,0 +1,13 @@
+package uz.jvh.avtoelonuzsayti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvtoElonUzSaytiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AvtoElonUzSaytiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/jvh/avtoelonuzsayti/AvtoElonUzSaytiApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.jvh.avtoelonuzsayti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvtoElonUzSaytiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
